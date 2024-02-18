e97309a65beed40475a5d54104608ecd522d32eb
Nazar Smutnitskiy <nsmutnitskiy@swivl.com>
2024-02-18 17:38:37 +0200

    add fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { concatenation } from './concatenation';
-
 const button = document.querySelector('button')!;
 const input = document.querySelector('input')!;
 
 if (button && input) {
     button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
+        console.log(`Hello ${input.value}`);
     });
 }
